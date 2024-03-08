a97b937e0887f9b7b3db34f13969bd2aa4af7c83
DESKTOP-MBVRGHE\admin <123456>
2024-03-08 18:05:46 +0800

    2024-03-08

--- src/views/map/data.js
+++ src/views/map/data.js
@@ -6,6 +6,78 @@ export const list = [
     image: 'https://nimg.ws.126.net/?url=http%3A%2F%2Fdingyue.ws.126.net%2F2022%2F0826%2Fe8bce663j00rh7fub0180d200u000hxg00f60092.jpg&thumbnail=660x2147483647&quality=80&type=jpg',
     point: [116.628441, 23.194694],
     page: '/my-dosc/docs/travel/潮汕游玩'
+  },
+  {
+    time: '2023-07-25',
+    title: '云南游玩',
+    desc: '云南八日游',
+    image: 'https://img1.baidu.com/it/u=3301450185,1761912848&fm=253&fmt=auto&app=138&f=JPEG?w=875&h=500',
+    point: [100.25, 26.86],
+    page: '/my-dosc/docs/travel/云南游玩'
+  },
+  {
+    time: '2023-10-03',
+    title: '澳门游玩',
+    desc: '澳门一日游',
+    image: 'https://img1.baidu.com/it/u=4294890346,2274444468&fm=253&fmt=auto&app=138&f=JPEG?w=750&h=500',
+    point: [113.54, 22.2],
+    page: '/my-dosc/docs/travel/澳门游玩'
+  },
+  {
+    time: '2019-07-16',
+    title: '香港游玩',
+    desc: '香港三日游',
+    image: 'https://img1.baidu.com/it/u=1989236381,2050966386&fm=253&fmt=auto&app=138&f=JPEG?w=749&h=500',
+    point: [114.18, 22.31],
+    page: '/my-dosc/docs/travel/香港游玩'
+  },
+  {
+    time: '2016-07-15',
+    title: '三亚游玩',
+    desc: '三亚暑假游玩',
+    image: 'https://img0.baidu.com/it/u=755020965,351662556&fm=253&fmt=auto&app=138&f=JPEG?w=680&h=447',
+    point: [109.5028, 18.25],
+    page: '/my-dosc/docs/travel/三亚游玩'
+  },
+  {
+    time: '2023-06-10',
+    title: '三明市 - 三天团建',
+    desc: '武阔天下 - 三明市团建',
+    image: 'https://img0.baidu.com/it/u=2391217593,1448719779&fm=253&fmt=auto&app=138&f=JPEG?w=708&h=500',
+    point: [117.65, 26.27],
+    page: '/my-dosc/docs/travel/三明团建'
+  },
+  {
+    time: '2024-02-09',
+    title: '泉州日常',
+    desc: '老家 - 泉州',
+    image: 'https://img2.baidu.com/it/u=2703826680,3226970461&fm=253&fmt=auto&app=138&f=JPEG?w=410&h=500',
+    point: [118.68, 24.86],
+    page: '/my-dosc/docs/travel/泉州日常'
+  },
+  {
+    time: '2024-03-07',
+    title: '厦门生活',
+    desc: '厦门 - 生活',
+    image: 'https://img0.baidu.com/it/u=3513705795,3222498820&fm=253&fmt=auto&app=120&f=JPEG?w=608&h=342',
+    point: [118.089, 24.4646],
+    page: '/my-dosc/docs/travel/厦门生活'
+  },
+  {
+    time: '2022-02-06',
+    title: '清远三日游',
+    desc: '广东清远游玩',
+    image: 'https://img1.baidu.com/it/u=2118600457,216769851&fm=253&fmt=auto&app=120&f=JPEG?w=607&h=369',
+    point: [113.0517, 23.6856],
+    page: '/my-dosc/docs/travel/清远游玩'
+  },
+  {
+    time: '2022-02-06',
+    title: '漳州游玩',
+    desc: '跟铁子们游玩',
+    image: 'https://img1.baidu.com/it/u=2118600457,216769851&fm=253&fmt=auto&app=120&f=JPEG?w=607&h=369',
+    point: [117.56, 23.94],
+    page: '/my-dosc/docs/travel/漳州游玩'
   }
 ]
 
